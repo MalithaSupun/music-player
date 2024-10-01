b777d59ddf2993ef6ab78fedcfad2e303acd0f50
Malitha Supun <malithasupun2@gmail.com>
2024-10-01 13:17:07 +0530

    Added SplashScreen and track player setup to App layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Stack } from 'expo-router'; 
+import { Stack, SplashScreen } from 'expo-router'; 
+import { useSetupTrackPlayer } from '@/hooks/useSetupTrackPlayer';
+import { useCallback } from 'react';
+import { useLogTrackPlayerState } from '@/hooks/useLogTrackPlayerState';
+
+SplashScreen.preventAutoHideAsync()
 
 const App = () => {
+
+	
+
+	const handleTrackPlayerLoaded = useCallback(() => {
+		SplashScreen.hideAsync()
+	}, [])
+
+	useSetupTrackPlayer({
+		onLoad: handleTrackPlayerLoaded,
+	})
+
+	useLogTrackPlayerState()
 	
 	return (
 		<SafeAreaProvider>
