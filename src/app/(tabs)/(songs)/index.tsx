a38e06fd9f3dc9cdccdd3bee51f16ac07973bf75
Malitha Supun <malithasupun2@gmail.com>
2024-10-01 10:57:37 +0530

    Updated SongsScreen and TrackList components with new styles and search functionality

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,11 +1,21 @@
 import { TrackList } from "@/components/TracksList";
+import { screenPadding } from "@/constants/tokens";
+import { useNavigationSearch } from "@/hooks/useNavigationSearch";
 import { defaultStyles } from "@/styles";
 import { ScrollView, Text, View } from "react-native";
 
 const SongsScreen = () => {
+    const search = useNavigationSearch({
+        searchBarOptions:{
+            placeholder: 'Find in Songs'
+        },
+    })
     return (
         <View style={defaultStyles.container}>
-            <ScrollView>
+            <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={{paddingHorizontal:screenPadding.horizontal}}
+            >
                 <TrackList scrollEnabled={false}/>
             </ScrollView>
         </View>
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,13 +1,22 @@
 import { FlatList } from 'react-native'
 import library from '@/assets/data/library.json'
 import { TrackListItem } from './TrackListitem'
+import { View } from 'react-native'
+import { utilsStyles } from '@/styles'
 
 export type TrackListProps = Partial<FlatList<unknown>>
+
+const itemDivider = () => (
+	<View style={{...utilsStyles.itemSeparator,marginVertical:9,marginLeft:60}} />
+)
 
 export const TrackList = ({...flatlistProps}: TrackListProps) => {
 	return (
 		<FlatList
 			data={library}
+			contentContainerStyle={{paddingTop:10, paddingBottom:128}}
+			ListFooterComponent={itemDivider}
+			ItemSeparatorComponent={itemDivider}
 			renderItem={({ item: track }) => (
 				<TrackListItem
 					track={{
